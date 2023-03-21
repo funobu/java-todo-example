77fcc5b2c0ffcb22ee6155f3341b714b845b4e1a
funobu <fukids34@gmail.com>
2023-03-21 22:40:32 +0900

    feat: Add Update and Delete method

--- src/main/java/funobu/todo/TodoApplication.java
+++ src/main/java/funobu/todo/TodoApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 @SpringBootApplication
@@ -39,6 +36,28 @@ public class TodoApplication {
         newTodo.setId(UUID.randomUUID().toString());
         this.todos.add(newTodo);
         System.out.println(newTodo.getContent());
+        return "redirect:/";
+    }
+
+    @PutMapping("/{id}")
+    public String updateTodoByID(@PathVariable String id, @RequestParam String content, Model model) {
+        todos.forEach(todo -> {
+            if (todo.getId().equals(id)) {
+                todo.setContent(content);
+            }
+        });
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTodoByID(@PathVariable String id, Model model) {
+        Iterator<Todo> iTodos = todos.iterator();
+        while (iTodos.hasNext()) {
+            Todo iTodo = iTodos.next();
+            if (iTodo.getId().equals(id)) {
+                iTodos.remove();
+            }
+        }
         return "redirect:/";
     }
 }
